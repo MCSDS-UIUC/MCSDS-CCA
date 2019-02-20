c583ff83094e01c6a2a25f20ec54ec0ceed69d48
Manas Mukherjee <mmanas19@gmail.com>
2019-02-20 00:48:51 -0800

    week4 - mapreduce using virtualbox and hortonwork-sandbox

---
+++ assignments/tutorials/week4/wordcount.py
@@ -0,0 +1,15 @@
+import sys
+from operator import add
+from pyspark import SparkContext
+import re
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("less args.check")
+        exit(-1)
+    sc = SparkContext(appName="PythonWordCount")
+    common_words = ["the", "a", "an", "and", "of", "to", "in", "am", "is", "are", "at", "not"]
+    lines = sc.textFile(sys.argv[1], 1)
+    counts = lines.flatMap(lambda x: re.split(r"[ \t,;\.\?!-:@\ [\]\(\){}_\*/]+", x)).filter(lambda x: x.lower() not in common_words and len(x) > 0).map(lambda x: (x, 1)).reduceByKey(add)
+    counts.saveAsTextFile(sys.argv[2])
+    sc.stop()
